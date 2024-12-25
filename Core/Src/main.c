73d93bb6832a2d0d0fe0d66a0053e15b52445c13
Igor Sivtsov <igor.sivtsov@portal59.ru>
2024-12-25 12:59:41 +0700

    Установка начального состояния индикации и подтяжка usb_dp

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,6 +96,9 @@ int main(void)
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
+  LED_GPIO_Port->BSRR = LED_Pin;
+  USB_CTRL_GPIO_Port->BSRR = USB_CTRL_Pin << 16;
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
